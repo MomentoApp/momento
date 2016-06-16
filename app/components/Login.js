f93f8bda439c6248ca7ab0d68d839e6d3c87d772
nlebedev <nlebedev100@gmail.com>
2016-06-15 20:19:08 -0700

    Implement basic react native structure

---
+++ app/components/Login.js
@@ -0,0 +1,33 @@
+import React, { Component} from 'react';
+import { 
+  View, 
+  StyleSheet,
+  Text 
+} from 'react-native';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>Hello Momento</Text>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  text: {
+    color: 'red',
+    flex: 1,
+    fontSize: 26,
+    textAlign: 'center',
+    marginTop: 100 
+  }
+});
+
+module.exports = Login;
