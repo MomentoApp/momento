e064fe66a7d7e0c50a6af2813322da2fce3b7601
Chris Choi <cmhchoi@gmail.com>
2016-06-16 16:56:50 -0700

    server up

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`);
+});
