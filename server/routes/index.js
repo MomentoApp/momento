9a2dfd68796f2e639916d49ccf9bc26907b60bb4
Chris Choi <cmhchoi@gmail.com>
2016-06-17 19:43:05 -0700

    updated seed, updated video controller typo

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 const videoController = require('../controllers/videoController');
+const videoController = require('../controllers/videowithinController');
 
 router.get('/user', userController.get);
 router.post('/user', userController.post);
 
 router.get('/video', videoController.get);
 router.post('/video', videoController.post);
+
+router.get('/videowithin', videowithinController.get);
 
 module.exports = router;
