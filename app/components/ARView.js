075a8b1118f196c78a99b7100b7c09ccc57e5234
Kamal Mango <kamal.mango7@gmail.com>
2016-06-18 15:42:13 -0700

    fix syntax

--- app/components/ARView.js
+++ app/components/ARView.js
@@ -91,48 +91,39 @@ const html = `<!DOCTYPE html>
 // this is a temporary test object that mimics the JSON that will be received from the db
 let testObj = [
   {
-    "id": 1,
-    "url": "https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4",
-    "point": {
-      "type": "Point",
-      "coordinates": [
-        37.7837221,
-        -122.4091839
-      ]
+    id: 1,
+    url: 'https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4',
+    point: {
+      type: 'Point',
+      coordinates: [37.7837221, -122.4091839],
     },
-    "createdAt": "2016-06-17T05:17:34.996Z",
-    "updatedAt": "2016-06-17T05:17:34.996Z",
-    "UserId": 1
+    createdAt: '2016-06-17T05:17:34.996Z',
+    updatedAt: '2016-06-17T05:17:34.996Z',
+    UserId: 1,
   },
   {
-    "id": 2,
-    "url": "https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4",
-    "point": {
-      "type": "Point",
-      "coordinates": [
-        37.7847912,
-        -122.40713522
-      ]
+    id: 2,
+    url: 'https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4',
+    point: {
+      type: 'Point',
+      coordinates: [37.7847912, -122.40713522],
     },
-    "createdAt": "2016-06-17T05:17:34.996Z",
-    "updatedAt": "2016-06-17T05:17:34.996Z",
-    "UserId": 1
+    createdAt: '2016-06-17T05:17:34.996Z',
+    updatedAt: '2016-06-17T05:17:34.996Z',
+    UserId: 1,
   },
   {
-    "id": 3,
-    "url": "https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4",
-    "point": {
-      "type": "Point",
-      "coordinates": [
-        37.74267,
-        -122.48634
-      ]
+    id: 3,
+    url: 'https://instagram.fsjc1-2.fna.fbcdn.net/t50.2886-16/13448244_1764286310482288_2066918794_n.mp4',
+    point: {
+      type: 'Point',
+      coordinates: [37.74267, -122.48634],
     },
-    "createdAt": "2016-06-17T05:17:34.996Z",
-    "updatedAt": "2016-06-17T05:17:34.996Z",
-    "UserId": 1
-  }
-]
+    createdAt: '2016-06-17T05:17:34.996Z',
+    updatedAt: '2016-06-17T05:17:34.996Z',
+    UserId: 1,
+  },
+];
 
 
 class ARView extends Component {
@@ -191,25 +182,24 @@ class ARView extends Component {
 
 
         {(() => {
-            var latitude = Number(Number(this.state.latitude).toFixed(3));
-            var longitude = Number(Number(this.state.longitude).toFixed(3));
+          const latitude = Number(Number(this.state.latitude).toFixed(3));
+          const longitude = Number(Number(this.state.longitude).toFixed(3));
 
-            return testObj.map(function(data) {
-              var lat = Number(Number(data.point.coordinates[0]).toFixed(3));
-              var longt = Number(Number(data.point.coordinates[1]).toFixed(3));
-              if ( (latitude === lat) && (longitude === longt) )  {
-                return (
-                  <View style={styles.webViewWrap}>
-                    <WebView
-                      style={styles.webView}
-                      source={{ html: html }}
-                    />
-                  </View>
-                );
-              }
-            });
+          return testObj.map(data => {
+            const lat = Number(Number(data.point.coordinates[0]).toFixed(3));
+            const longt = Number(Number(data.point.coordinates[1]).toFixed(3));
+            if ((latitude === lat) && (longitude === longt)) {
+              return (
+                <View style={styles.webViewWrap}>
+                  <WebView
+                    style={styles.webView}
+                    source={{ html: html }}
+                  />
+                </View>
+              );
+            }
+          });
         })()}
-        
         <Nav currentPage="ar" />
       </View>
     );
