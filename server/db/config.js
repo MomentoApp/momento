17936e0be2e545a045c1e6252c804965e76aa050
Chris Choi <cmhchoi@gmail.com>
2016-06-21 23:59:51 -0700

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  username: 'momento',
-  password: 'Hackreactor',
-  database: 'momento',
-  config: {
-    host: 'momento.caxamxz9bjrd.us-west-2.rds.amazonaws.com',
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-  },
-};
+
