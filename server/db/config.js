549e7fc57ea30edb9abb67ae8cf8ca0b9474b91b
Chris Choi <cmhchoi@gmail.com>
2016-06-22 17:03:42 -0700

    db config with process env

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  username: 'momento',
-  password: 'Hackreactor',
-  database: 'momento',
+  username: process.env.db_username,
+  password: process.env.db_password,
+  database: process.env.db_database,
   config: {
-    host: 'momento.caxamxz9bjrd.us-west-2.rds.amazonaws.com',
+    host: process.env.db_host,
     dialect: 'postgres',
     pool: {
       max: 5,
